062f5255c0ec83fb4d9c81ccc8c4e923da575b71
iameeo <joowon1028@gmail.com>
2019-09-12 19:44:28 +0900

    setting

---
+++ wemarry/blog/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, redirect
+from django.core.mail import EmailMessage
+from django.core.files.storage import FileSystemStorage
+from django.conf import settings
+import os
+
+def index(request):
+    if request.method == 'GET':
+        context = {     }
+        return render(request, 'index.html', context)
+
+def j45ljmm5ec(request):
+    if request.method == 'GET':
+        context = {     }
+        return render(request, 'j45ljmm5ec.html', context)
